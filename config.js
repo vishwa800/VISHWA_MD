963a7148fb011634f047d77cd443c70d8b362972
vishwa800 <165990802+vishwa800@users.noreply.github.com>
2024-08-22 17:26:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID=`4H9hnBLb#Qur3sxU8nc5UvdXbEQrUkhpfWuaFpdGAWC80S0FcJ0Q`,
+SESSION_ID: process.env.SESSION_ID=`jPRwnDoI#1c6ezTlRV7Io4ksK8khqTmVMFJ3npHXjUzP5KKIjytY`,
 ALIVE_MSG: process.env.ALIVE_MSG=`Hi I AM ALIVE NOW👋`,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/68d6393c24646149a2a47.jpg",
 
