f9f337b2fb2e2d516e4f05feea119cfaf3c94ff4
vishwa800 <165990802+vishwa800@users.noreply.github.com>
2024-08-24 17:14:05 +0530

    Add files via upload

---
+++ plugins/menu (1).js
@@ -0,0 +1,63 @@
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "menu",
+    desc: "Tharusha md whatsapp bot cmd list.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+const config = await readEnv();
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
+};
+  
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `${config.PREFIX}${commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = `👏 *Hello ${pushname}
+> *DOWNLOAD COMMANDS* ✅
+
+${menu.download}
+
+> *MAIN COMMANDS* ✅
+
+${menu.main}
+
+>*GROUP COMMANDS* ✅
+
+${menu.group}
+
+>*OWNER COMMANDS* ✅
+
+${menu.owner}
+
+>*CONVERT COMMANDS* ✅
+
+${menu.convert}
+
+>*SEARCH COMMANDS* ✅
+
+${menu.search}
+
+POWERD BY THARUSHA SANDIPA 💚
+`
+await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
+
+  
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
