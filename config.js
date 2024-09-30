d3566520dffd9e75c908755ba4ae97fb46f1f349
vishwa800 <165990802+vishwa800@users.noreply.github.com>
2024-09-30 13:06:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID=`bO4DXDBI#pfskUH_gJJ3TPyRqBcO7ya4orsQXFJ2rS8EFxbBzmA4`,
+SESSION_ID: process.env.SESSION_ID=`XW4hRDqZ#0lLUEfJ7KikKvRY46qLxUCEzqwgm6TVme4fBaDTeyiU`,
 ALIVE_MSG: process.env.ALIVE_MSG=`Hi I AM ALIVE NOW👋
 
-♤𝐈 𝐀𝐌 𝐕𝐈𝐒𝐇𝐖𝐀-𝐌𝐃 𝐌𝐔𝐋𝐓𝐈𝐃𝐄𝐕𝐈𝐂𝐄 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐁𝐎𝐓♢🐰 https://chat.whatsapp.com/EEDH68vR6YT4WE7Otu4kqD
+♤𝐈 𝐀𝐌 𝐕𝐈𝐒𝐇𝐖𝐀-𝐌𝐃 𝐌𝐔𝐋𝐓𝐈𝐃𝐄𝐕𝐈𝐂𝐄 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐁𝐎𝐓♢🐰
+
+𝐎𝐖𝐍𝐄𝐑 𝐕𝐈𝐒𝐇𝐖𝐀 𝐊𝐎𝐃𝐈𝐓𝐇𝐔𝐖𝐀𝐊𝐊𝐔☘
+https://chat.whatsapp.com/EEDH68vR6YT4WE7Otu4kqD
 
 > ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴠɪꜱʜᴡᴀ ᴋᴏᴅɪᴛʜᴜᴡᴀᴋᴋᴜ☠`,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/68d6393c24646149a2a47.jpg",
