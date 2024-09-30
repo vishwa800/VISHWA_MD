f8e5189903aed91b2b978e05ecad6ac086ad5c73
vishwa800 <165990802+vishwa800@users.noreply.github.com>
2024-09-30 12:12:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID=`ZB4WyZKZ#FH5ZNgpDsAWOXlmFbnILtcOpWaZDWefuzT1zOpqwsGA`,
+SESSION_ID: process.env.SESSION_ID=`fOYgXSab#SusN5zMpznYhBBy8CWxY40JHX1__k_IC9XkqhXFz2sU`,
 ALIVE_MSG: process.env.ALIVE_MSG=`Hi I AM ALIVE NOW👋
 
 ♤𝐈 𝐀𝐌 𝐕𝐈𝐒𝐇𝐖𝐀-𝐌𝐃 𝐌𝐔𝐋𝐓𝐈𝐃𝐄𝐕𝐈𝐂𝐄 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐁𝐎𝐓♢
